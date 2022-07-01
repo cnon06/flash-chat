977ab905d5ec2afc7727c910ccce45dfb35de7d6
sinan <sinanalagoz74@gmail.com>
2022-07-01 09:22:36 +0300

    fddf

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//tfhgghgh
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
