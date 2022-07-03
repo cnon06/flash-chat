8c9a8ec11883e71c02a8c529186148a44b3576af
sinan <sinanalagoz74@gmail.com>
2022-07-03 17:03:20 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
   final firestore = FirebaseFirestore.instance;
 
   @override
@@ -53,10 +52,37 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+                stream: firestore.collection("messages").snapshots(),
+                builder: (context, snapshot) {
+                  List<Text> messageWidgets = [];
+                  if (snapshot.hasData) {
+                    final messages = snapshot.data?.docs;
 
+                    for (var msg in messages!) {
+                      final messageText = msg.get("text");
+                      final messageSender = msg.get("sender");
+                      final messageWidget =
+                          Text("$messageText from $messageSender");
+                      messageWidgets.add(messageWidget);
+                    }
+                   
+                    print("dsddd: $messages");
+                     
+                  }
+
+                //return Text("fgfg");
+                  
+                  
+                  return Column(
+                    children:messageWidgets,
+                  );
+                  
+                  
+                  
+                }),
             TextButton(
               onPressed: () async {
-                
                 await for (var snapshot
                     in firestore.collection("messages").snapshots()) {
                   for (var msg in snapshot.docs) {
